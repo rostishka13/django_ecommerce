1e4e651d304dfe76c5c54424ffad44b34504e0ce
Rostyslav Kamyshanov <rostishka1@gmail.com>
2020-12-11 10:39:04 +0200

    product innitial

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+	title = models.CharField(max_length = 255, null = False)
+	price = models.FloatField()
+	information = models.TextField()
+	image = models.ImageField(null = True, blank = True)
+	#rating
+
+	def __str__(self):
+		return self.title
